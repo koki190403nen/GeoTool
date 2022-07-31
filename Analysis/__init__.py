48a944ad15ba48ed21e55d7703b83899056e0322
Koki UDA <koki190403nen.3@gmail.com>
2022-07-31 12:12:13 +0900

    [pdate] GetNDVIArrを追加()

--- Analysis/__init__.py
+++ Analysis/__init__.py
@@ -1,3 +1,4 @@
 from .SpecifyCoodinatesSinusoidal import SpecifyCoodinatesSinusoidal
 from .MoveFunction import MoveFunction
 from .CalcTempNormalParams import calc_temp_normal_params
+from .GetNDVIArr import GetNDVIArr
--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from . import Convert
 from . import Analysis
 from .Statistics import PiecewiseRegression
+from .Analysis import GetNDVIArr
 __version__ = '1.0.0'
