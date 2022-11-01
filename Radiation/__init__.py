d92856f5214a27df262829e478ff7189130ee19d
koki uda <koki190403nen.3@gmail.com>
2022-11-01 11:20:17 +0900

    [update]

--- Radiation/__init__.py
+++ Radiation/__init__.py
@@ -1,2 +1,2 @@
 from .calc_sinh import calc_sinh
-from .ProcessWsiImage import ProcessWsiImage, MultiWsiImage,get_circle_img
+from .ProcessWsiImage import ProcessWsiImage, MultiWsiImage,get_circle_img, split_sort_df
