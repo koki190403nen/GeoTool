264cbe2b323b1b9bd5f5c1c0da0483442da8048a
Koki UDA <koki190403nen.3@gmail.com>
2023-06-22 17:26:58 +0900

    [DEL] delete rasterio

--- Convert/__init__.py
+++ Convert/__init__.py
@@ -1,7 +1,6 @@
 from .arr2tif import arr2tif
 from .nc2tif import Nc2Tif
 from .base import Convert
-from .get_metadata import get_metadata
 from .Raster2Dict import Raster2Dict
 from .Raster2Arr import Raster2Arr
 from .Extract_1point_items import Extract_1point_items
