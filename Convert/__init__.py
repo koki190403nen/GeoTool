4f8d71403deb8e0487ef1b1430f5a6d6700e8052
Koki UDA <koki190403nen.3@gmail.com>
2023-07-28 22:47:57 +0900

    [UPGRADE] MergeTransを追加

--- Convert/__init__.py
+++ Convert/__init__.py
@@ -4,3 +4,4 @@ from .base import Convert
 from .Raster2Dict import Raster2Dict
 from .Raster2Arr import Raster2Arr
 from .Extract_1point_items import Extract_1point_items
+from .MergeTrans import MergeTrans
