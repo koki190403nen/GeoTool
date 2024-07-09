9c5074df0e67c3393123e620124c04d41adf8699
kokiuda <b0885@n-koei.co.jp>
2024-07-09 09:58:49 +0900

    [fix]

--- Convert/MakeProjectDirectory.py
+++ Convert/MakeProjectDirectory.py
@@ -14,8 +14,10 @@ def MakeProjectDirectory():
 
     [os.makedirs(f'.//02_DATA//Level{i}', exist_ok=True) for i in range(4)]
 
-    with open(f'.//test.py', 'a') as f:
-        pass
-    with open(f'.//test2.py', 'a') as f:
-        pass
+    if os.path.isfile('../test.py'):
+        with open(f'.//test.py', 'a') as f:
+            pass
+    if os.path.isfile('../test2.py'):
+        with open(f'.//test2.py', 'a') as f:
+            pass
 
